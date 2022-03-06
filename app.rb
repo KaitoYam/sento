ec7bba899a7ee763fac4156f76c07009242db728
KaitoYam <k.yamaguchi.reiwa00@gmail.com>
2022-03-06 12:00:08 +0000

    サインアップ実装

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require 'sinatra'
 require './models'
 enable :session
 
+helpers do
+    def current_user
+        User.find_by(id: session[:user])
+    end
+end
 
 get '/' do
   erb :index
@@ -13,5 +18,23 @@ get '/signup' do
 end
 
 post '/signup' do
-    
+    confirm = User.find_by(name: params[:name])
+    if confirm.nil?
+        user = User.create(
+            name: params[:name],
+            password: params[:password],
+            password_confirmation: params[:password_confirmation],
+            point: 0,
+            rank: "normal"
+        )
+        if user.persisted?
+            session[:user] = user.id
+        end
+        redirect '/main'
+    end
+    redirect '/signup'
+end
+
+get '/main' do
+    erb :main
 end
--- models.rb
+++ models.rb
@@ -2,3 +2,10 @@ require 'bundler/setup'
 Bundler.require
 
 ActiveRecord::Base.establish_connection
+
+class User < ActiveRecord::Base
+    has_secure_password
+    validates :password,
+        length: {in: 5..15}
+end
+
